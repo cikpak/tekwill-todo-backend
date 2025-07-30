ea6890524d854cc4f900c5c9858b2225ec7ac3e8
simion gorgos <simion.gorgos@gmail.com>
2025-07-30 17:43:15 +0300

    Changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser')
 
-const app = express();x
+const app = express();
 const db = require('./config/db');
 
 const cors = require('cors'); // Import the cors package
@@ -14,8 +14,6 @@ const User = require('./models/User');
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded())
-app.use(bodyParser.json())
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -45,7 +43,7 @@ const login = async (req, res) => {
 };
 
 const getTodos = async (req, res) => {
-  const todos = await Todo.findAll();
+  const todos = await Todo.findAll({ where: { owner: req.user.id } });
   res.json(todos);
 };
 
@@ -74,6 +72,30 @@ const createTodo = async (req, res) => {
   res.status(201).json(todo);
 };
 
+const patchTodo = async (req, res) => {
+  const { id } = req.params;
+  const body = req.body
+
+  try {
+      const todo = await Todo.findOne({ where: { id, owner: req.user.id } })
+
+      if(todo == null) {
+        return res.status(404).json({ error: 'Todo not found' });
+      }
+      
+      const updatedTodo = {
+        ...todo.toJSON(),
+        ...body,
+      }
+
+      await todo.update(updatedTodo);
+
+      res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).json({error: 'Failed to mark todo as complete!'});
+  }
+}
+
 const authMiddleware = async (req, res, next) => {
   const userId = req.headers['current-user']
 
@@ -81,6 +103,10 @@ const authMiddleware = async (req, res, next) => {
     return res.status(401).json({ error: 'Invalid credentials' });
   }else{
     const user = await User.findOne({ where: { id: userId } });
+
+    if (user == null) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
 
     req.user = user;
   }
@@ -93,6 +119,7 @@ router.post('/login', login);
 
 router.get('/todos', [authMiddleware], getTodos);
 router.post('/todos', [authMiddleware], createTodo);
+router.patch('/todos/:id', [authMiddleware], patchTodo);
 
 app.use('/', router);
 
